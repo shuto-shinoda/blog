4d9d15d85bbb57bed5842f3a90a36dd2d133aa8b
shuto-shinoda <shuto.88.tb@gmail.com>
2021-01-14 15:34:08 +0900

    詳細検索
    ・検索ワード
    ・投稿日
    ・タグ
    
    heaterの検索barを削除

--- blog/settings.py
+++ blog/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog_app.tag_list.common',
             ],
             'builtins':[ 
                'bootstrap4.templatetags.bootstrap4',
